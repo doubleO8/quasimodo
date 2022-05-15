1fcda867dbd8d19f344d75fc3e9d4e1446035722
doubleO8 <wb008@hdm-stuttgart.de>
2022-05-15 11:58:34 +0200

    module version detection

--- quasimodo/__init__.py
+++ quasimodo/__init__.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    try:
+        from importlib.metadata import version
+    except ImportError:
+        from importlib_metadata import version
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+    __version__ = version(__package__)
+except Exception:
+    __version__ = "0.0.999"
